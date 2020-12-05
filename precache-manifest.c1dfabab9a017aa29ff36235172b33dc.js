4348b2489f089baf748f584d6145fc9335b63eb9
Sergei Ignatiev <Immortal230719@gmail.com>
2020-12-05 22:00:40 +0300

    Updates

---
+++ precache-manifest.c1dfabab9a017aa29ff36235172b33dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bd967c6d329ca51d4029062ef9b840",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a0f7102164abaa5e686b",
+    "url": "./static/css/main.8188e285.chunk.css"
+  },
+  {
+    "revision": "861b96908726976995e5",
+    "url": "./static/js/2.fb722b6c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.fb722b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0f7102164abaa5e686b",
+    "url": "./static/js/main.ec34eb3f.chunk.js"
+  },
+  {
+    "revision": "cffef606768cc0244c19",
+    "url": "./static/js/runtime-main.ab30e8c0.js"
+  },
+  {
+    "revision": "b9693c51250542799a5e135754ba5214",
+    "url": "./static/media/cat.b9693c51.png"
+  }
+]);
